782c7a993c6f550219a1eb1afbd00f5fb80fbe5e
rtagliavia <rtagliavia.pp@gmail.com>
2020-10-28 11:08:31 -0400

    Updates

---
+++ precache-manifest.9dd229fc194566825554867c9f92bf60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c87ff53882c9acbf3d765500df29ebd",
+    "url": "/React-OMDB/index.html"
+  },
+  {
+    "revision": "0f2cded14d2094d428a6",
+    "url": "/React-OMDB/static/css/main.3620d4eb.chunk.css"
+  },
+  {
+    "revision": "b9b7f19d7da20bfe692d",
+    "url": "/React-OMDB/static/js/2.4e02f597.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-OMDB/static/js/2.4e02f597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2cded14d2094d428a6",
+    "url": "/React-OMDB/static/js/main.96b5e94d.chunk.js"
+  },
+  {
+    "revision": "7425d82af7f829a34d14",
+    "url": "/React-OMDB/static/js/runtime-main.47dad443.js"
+  }
+]);
