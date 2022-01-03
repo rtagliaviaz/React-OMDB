dc5b3fd585bdf1a9e8f8b2e791aee8a013c86218
unknown <rtagliavia.pp@gmail.com>
2022-01-03 18:36:46 -0400

    https

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const MovieModal = ({movieInfo, handleClose}) => {
-<<<<<<< HEAD
 
-=======
-  console.log('asda', movieInfo)
->>>>>>> 7dd6ca833c9f233954c03134ee6a7786b9fd6cf8
   return (
     <div className="modal display-block">
                 <section className="modal-main">
